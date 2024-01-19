2ad0f4aab94e0016e7dfdd2a325791eb8a8584d5
Hussen Jamoul <jamoulh312@gmail.com>
2024-01-19 13:10:58 +0100

    [ADD] migrations command to execute in specific order

---
+++ app/Console/Commands/MigrateInOrder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MigrateInOrder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate_in_order';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Execute the migrations in the order specified in the file app/Console/Comands/MigrateInOrder.php \n Drop all the table in db before execute the command.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $migrations = [
+            '2024_01_18_221634_concrete_blogs.php',
+            '2023_12_25_235143_create_abstract_blogs_table.php',
+        ];
+
+        foreach ($migrations as $migration) {
+            $basePath = 'database/migrations/';
+            $migrationName = trim($migration);
+            $path = $basePath . $migrationName;
+            $this->call('migrate:refresh', [
+                '--path' => $path,
+            ]);
+        }
+    }
+
+}
