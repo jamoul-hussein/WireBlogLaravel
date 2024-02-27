d57ee2bca9e762e59780b1cb64fd010643c63db7
Hussen Jamoul <jamoulh312@gmail.com>
2024-02-27 20:59:11 +0100

    [UPDATE][WIP] Add account bar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,37 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wire Blog</title>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/startPage.css') }}" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/startPage.css') }}">
 </head>
 <body>
 <section class="layout">
-    <div class="sidebar">
+    <aside>
+        <div class="sidebar">
+            <nav class="sidebar-nav" id="sidebar-nav">
+                <h1 class="text-center">Wire Blog</h1>
+                <ul>
+                    <li class="sidebar-nav-item active"><a href="/startPage" class="sidebar-nav-link">
+                            <i></i>
+                            Blog Home
+                        </a></li>
+                    <li class="sidebar-nav-item"><a href="/post/blog" class="sidebar-nav-link">
+                            <i></i>
+                            Post a Blog
+                        </a></li>
+                    <li class="sidebar-nav-item"><a href="/" class="sidebar-nav-link">
+                            <i></i>
+                            About Wire
+                        </a></li>
+                    <li class="sidebar-nav-item"><a href="/" class="sidebar-nav-link">
+                            <i class="far fa-comments"></i>
+                            Contact Us
+                        </a></li>
+                </ul>
+            </nav>
+        </div>
+    </aside>
 
-        <h1 class="text-center">Wire Blog</h1>
-
-        <nav class="sidebar-nav" id="sidebar-nav">
-            <ul>
-                <li class="sidebar-nav-item active"><a href="/startPage" class="sidebar-nav-link">
-                        <i></i>
-                        Blog Home
-                    </a></li>
-                <li class="sidebar-nav-item"><a href="/post/blog" class="sidebar-nav-link">
-                        <i></i>
-                        Post a Blog
-                    </a></li>
-                <li class="sidebar-nav-item"><a href="/" class="sidebar-nav-link">
-                        <i></i>
-                        About Wire
-                    </a></li>
-                <li class="sidebar-nav-item"><a href="/" class="sidebar-nav-link">
-                        <i class="far fa-comments"></i>
-                        Contact Us
-                    </a></li>
-            </ul>
-        </nav>
-    </div>
-
-       @yield('main')
+@yield('main')
 </body>
 </html>
--- resources/views/startPage.blade.php
+++ resources/views/startPage.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.master')
 
 @section('main')
+    <section>
         <div class="body">
+            <div class="account-bar">
 
+            </div>
             <div class="abstract-blogs-container">
-            @if($abstractBlogs)
-                @foreach($abstractBlogs as $abstractBlog)
-                    @include('components.abstract-blog' , [
-                             'abstractBlogId' => $abstractBlog->id,
-                             'abstractBlogName' => $abstractBlog->name,
-                             'abstractBlogDescription' => $abstractBlog->description,
-                             'abstractBlogCategory'=>  $abstractBlog->category,
-                             'abstractBlogCreatedAt' => $abstractBlog->created_at,
-                             'imageLink' => $abstractBlog->imagesBlog->image_link,
-                    ])
-                @endforeach
+                @if($abstractBlogs)
+                    @foreach($abstractBlogs as $abstractBlog)
+                        @include('components.abstract-blog' , [
+                                 'abstractBlogId' => $abstractBlog->id,
+                                 'abstractBlogName' => $abstractBlog->name,
+                                 'abstractBlogDescription' => $abstractBlog->description,
+                                 'abstractBlogCategory'=>  $abstractBlog->category,
+                                 'abstractBlogCreatedAt' => $abstractBlog->created_at,
+                                 'imageLink' => $abstractBlog->imagesBlog->image_link,
+                        ])
+                    @endforeach
                 @endif
 
             </div>
