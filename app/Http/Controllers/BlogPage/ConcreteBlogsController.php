2f70077e073a53128311006d08f840a1606d1887
Hussen Jamoul <jamoulh312@gmail.com>
2024-03-07 17:28:09 +0100

    [COMPLETE] Concrete Blog article page

--- app/Http/Controllers/BlogPage/ConcreteBlogsController.php
+++ app/Http/Controllers/BlogPage/ConcreteBlogsController.php
@@ -5,36 +5,45 @@ namespace App\Http\Controllers\BlogPage;
 use App\Http\Controllers\Controller;
 use App\Models\AbstractBlogs;
 use App\Models\ConcreteBlogs;
-use Illuminate\Http\Request;
+use App\Models\ImagesBlogs;
 
 class ConcreteBlogsController extends Controller
 {
     public function index($concreteBlogId)
     {
-
         $abstractBlogs = AbstractBlogs::all()->where('concrete_blogs_id', '=', $concreteBlogId);
 
         $blogTitle = '';
         $blogCategory = '';
+        $blogImageFk = '';
         foreach ($abstractBlogs->toArray() as $abstractBlog) {
             $blogTitle = $abstractBlog['name'];
             $blogCategory = $abstractBlog['category'];
+            $blogImageFk = $abstractBlog['images_blogs_id'];
         }
 
-        $concreteBlog = ConcreteBlogs::all()->where('id','=', $concreteBlogId);
+        $concreteBlog = ConcreteBlogs::all()->where('id', '=', $concreteBlogId);
         $content = '';
         $created_at = '';
 
-        foreach ($concreteBlog->toArray() as $concreteBlogColumn){
+        $blogImage = ImagesBlogs::all()->where('id', '=', $blogImageFk);
+
+        foreach ($blogImage->toArray() as $image) {
+            $imageLink = $image['image_link'];
+        }
+
+        foreach ($concreteBlog->toArray() as $concreteBlogColumn) {
             $content = $concreteBlogColumn['content'];
             $created_at = $concreteBlogColumn['created_at'];
         }
+
         return view('concrete-blog', [
             'id' => $concreteBlogId,
             'content' => $content,
             'created_at' => $created_at,
             'blogTitle' => $blogTitle,
             'blogCategory' => $blogCategory,
+            'blogImageLink' => $imageLink,
         ]);
     }
 }
--- resources/views/concrete-blog.blade.php
+++ resources/views/concrete-blog.blade.php
@@ -1,12 +1,44 @@
-<div class="mb-4">
+@extends('layouts.master')
 
-    <h2 > {!! $blogTitle !!} </h2>
+@section('main')
+    <div class="bg-white sm:py-32">
+        <div class="mx-auto lg:px-8">
+            <div class="mx-auto max-w-5xl lg:mx-0">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{!! $blogTitle !!}</h2>
+                <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert
+                    advice.</p>
+            </div>
+            <div
+                class="mx-auto mt-10 max-w-5xl">
+                <article class="flex flex-col items-start justify-between">
+                    <div class="flex items-center gap-x-4 text-m">
+                        <div class="text-gray-500">{!! $created_at !!}</div>
+                        <a href="#"
+                           class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{!! $blogCategory !!}</a>
+                    </div>
 
-    <h4 > {!! $blogCategory !!} </h4>
+                    <div class="group relative">
+                        <img class="mt-5" src="{{ $blogImageLink }}">
+                        <p class="mt-5 text-m text-gray-600">
+                            {!! $content !!}
+                        </p>
+                    </div>
 
-    <p>{!! $created_at !!}</p>
-    <p>
-        {!! $content !!}
-    </p>
-    <span class="d-block text-right tm-color-primary">Creative . Design . Business</span>
-</div>
+                    <div class="relative mt-8 flex items-center gap-x-10 mt-5">
+                        <img
+                            src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt="" class="h-10 w-10 rounded-full">
+                        <div class="text-sm">
+                            <p class="font-semibold text-gray-900">
+                                <a href="#">
+                                    Max Foster
+                                </a>
+                            </p>
+                            <p class="text-gray-600">Co-Founder / CTO</p>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+@endsection
